98810f024a6c7020d3249d781e3c9d664f48d070
Rodrigo <129321935+RodrLH@users.noreply.github.com>
2023-11-19 13:23:15 -0500

    Delete assets directory

--- assets/js/main.js
+++
@@ -1,7 +0,0 @@
-const contacto = document.querySelector("#contacto");
-
-contacto.addEventListener("click", (c) =>{
-    c.preventDefault();
-    const sectionC = document.querySelector(".contacto");
-    sectionC.scrollIntoView({behavior:"smooth"});
-})
