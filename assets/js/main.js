865fb7d05d074b7de3c6f20b1818fa466a7f87aa
Rodrigo <u202212338@upc.edu.pe>
2023-11-19 17:34:53 -0500

    landing page changes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,11 +1,15 @@
 const contacto = document.querySelector("#contacto");
-
+const enviar = document.getElementById("btnEnviar");
 contacto.addEventListener("click", (c) =>{
     c.preventDefault();
     const sectionC = document.querySelector(".contacto");
     sectionC.scrollIntoView({behavior:"smooth"});
 })
-
+enviar.addEventListener("click", (e) =>{
+    e.preventDefault();
+    const sectionE = document.querySelector(".banner");
+    sectionE.scrollIntoView({behavior:"smooth"});
+})
 function scrolll(){
     const left = document.querySelector(".scroll-test");
     left.scrollBy(-350,0);
@@ -14,3 +18,8 @@ function scrollr(){
     const right = document.querySelector(".scroll-test");
     right.scrollBy(350,0);
 }
+window.addEventListener("load",function (){
+    document.getElementById("btnEnviar").addEventListener("click",function (){
+        alert("¡Gracias por escribirnos!");
+    })
+})
