01e180be6ad6d8e2fd9298dcd93fbf2b6ee51e70
Rodrigo <u202212338@upc.edu.pe>
2023-11-19 17:06:28 -0500

    beneficios, testimonios y contacto update

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,3 +5,12 @@ contacto.addEventListener("click", (c) =>{
     const sectionC = document.querySelector(".contacto");
     sectionC.scrollIntoView({behavior:"smooth"});
 })
+
+function scrolll(){
+    const left = document.querySelector(".scroll-test");
+    left.scrollBy(-350,0);
+}
+function scrollr(){
+    const right = document.querySelector(".scroll-test");
+    right.scrollBy(350,0);
+}
