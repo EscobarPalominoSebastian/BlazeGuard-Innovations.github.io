7b9db28df5575e46b4c82c1c21a6a7975e50f22d
EscobarPalominoSebastian <151480638+EscobarPalominoSebastian@users.noreply.github.com>
2023-11-19 20:10:10 -0500

    Update js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-const contacto = document.querySelector("#contacto");
+const contactoo = document.querySelector("#contacto");
 const enviar = document.getElementById("btnEnviar");
 contacto.addEventListener("click", (c) =>{
     c.preventDefault();
